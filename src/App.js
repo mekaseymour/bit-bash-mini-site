f400270fdb947b70edcb425b65b99c6425945bcc
Meka Seymour <mekaseymour@gmail.com>
2019-09-02 22:26:14 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import activeGameImage from './activeGame.svg'
+import wonGameImage from './wonGame.svg'
 
-function App() {
+import PrivacyPolicy from './PrivacyPolicy';
+
+const MAX_WIDTH_FOR_SMALL_SCREEN_SIZE = 768;
+
+const getWindowWidth = () => window.innerWidth;
+
+const Index = () => {
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  const handleWindowResize = () => {
+    setWindowWidth(getWindowWidth())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={container}>
+      <h1 style={header(windowWidth)}>Bi+ B×sh</h1>
+      <h3 style={subHeader(windowWidth)}>Put your mental math to the test</h3>
+      <div style={screenshotsContainer(windowWidth)}>
+        <img style={screenshot(windowWidth)} src={activeGameImage} />
+        <img style={screenshot(windowWidth)} src={wonGameImage} />
+      </div>
+
+      <div style={footer(windowWidth)}><Link style={link} to="/privacy/">Privacy Policy</Link></div>
     </div>
   );
 }
+
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Index} />
+  <Route path="/privacy/" component={PrivacyPolicy} />
+</Router>
+  )
+
+const container = {
+  display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: '#4681FF',
+  minHeight: '100vh',
+  height: '100%',
+  alignItems: 'center',
+  padding: '20px'
+}
+
+const footer = windowWidth => ({
+  display: 'flex',
+  marginTop: '30px',
+  justifySelf: 'flex-end',
+  width: '100%',
+  fontFamily: 'Bungee',
+  flexDirection: windowWidth > MAX_WIDTH_FOR_SMALL_SCREEN_SIZE ? 'row' : 'column',
+  height: '50px',
+  alignItems: 'center',
+  fontSize: '12px',
+})
+
+const header = windowWidth => ({
+  fontFamily: 'Bungee Shade',
+  fontSize: windowWidth > MAX_WIDTH_FOR_SMALL_SCREEN_SIZE ? '60px' : '40px',
+})
+
+const link = {
+  color: 'black',
+}
+
+const screenshotsContainer = windowWidth => ({
+  display: 'flex',
+  flexDirection: windowWidth > MAX_WIDTH_FOR_SMALL_SCREEN_SIZE ? 'row' : 'column',
+  marginTop: '30px',
+})
+
+const screenshot = windowWidth => ({
+  height: windowWidth > MAX_WIDTH_FOR_SMALL_SCREEN_SIZE ? '600px' : '500px',
+})
+
+const subHeader = windowWidth => ({
+  fontFamily: 'Bungee',
+  fontSize:  windowWidth > MAX_WIDTH_FOR_SMALL_SCREEN_SIZE ? '18px' : '16px',
+  margin: 0,
+})
 
 export default App;
